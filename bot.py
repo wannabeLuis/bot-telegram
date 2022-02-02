b1261cf41ee804ea49ce628992a92aab0a226d0d
Jose Luis Hernandez <78991185+wannabeLuis@users.noreply.github.com>
2022-02-01 22:04:30 -0600

    Hola mundo

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+
+    update.message.reply_text('Hola mundo!')
+
+if __name__ == '__main__':
+
+    updater = Updater(token='TOKEN', use_context='True')
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+
+    updater.start_polling()
+    updater.idle()
