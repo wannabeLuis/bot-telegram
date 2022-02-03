657d171306ac1b824c7d868884ec94eb6dcb5730
Jose Luis Hernandez <78991185+wannabeLuis@users.noreply.github.com>
2022-02-03 16:02:25 -0600

    git push stash

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ def input_text(update, context):
 
 if __name__ == '__main__':
 
-    updater = Updater(token='904298539:AAEEiVXBPjOF0LC0rNvT2OECqPrdZ3OLW4M', use_context='True')
+    updater = Updater(token='TOKEN', use_context='True')
 
     dp = updater.dispatcher
 
